70cb8097009a570918b5c562ee5f60301344b6e3
Krystian Ciamaga <krystianciamaga@gmail.com>
2020-06-23 10:34:55 +0200

    .

---
+++ src/main/java/krystianciamaga/com/demo/settings/RestClientService.java
@@ -0,0 +1,29 @@
+package krystianciamaga.com.demo.mocks;
+
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.RestClients;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RestClientService {
+
+
+
+
+
+    @Bean
+    public RestHighLevelClient client() {
+        ClientConfiguration clientConfiguration
+                = ClientConfiguration.builder()
+                .connectedTo("localhost:9200")
+                .build();
+
+        return RestClients.create(clientConfiguration).rest();
+    }
+
+
+
+
+}
